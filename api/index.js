50b1598fdd93d263ab80c5edbd2a22baec4dc36f
rafaelbc-dev <rafaelbc.dev@gmail.com>
2023-01-08 13:18:07 +0100

    More functionality added.

--- api/index.js
+++ api/index.js
@@ -8,7 +8,10 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/serve-static'
 import leaderboard from '../db/leaderboard.json'
+import teams from '../db/teams.json'
+import presidents from '../db/presidents.json'
 
 const app = new Hono()
 
@@ -16,7 +19,15 @@ app.get('/', (ctx) => {
   return ctx.json([
     {
       endpoint: '/leaderboard',
-      description: 'Returns de leaderboard'
+      description: 'Returns the Kings League leaderboard'
+    },
+    {
+      endpoint: '/teams',
+      description: 'Returns the Kings League teams'
+    },
+    {
+      endpoint: '/presidents',
+      description: 'Returns the Kings League presidents'
     }
   ])
 })
@@ -24,6 +35,23 @@ app.get('/', (ctx) => {
 app.get('/leaderboard', (ctx) => {
   return ctx.json(leaderboard)
 })
+
+app.get('/teams', (ctx) => {
+  return ctx.json(teams)
+})
+
+app.get('/presidents', (ctx) => {
+  return ctx.json(presidents)
+})
+
+app.get('/presidents/:id', (ctx) => {
+  const foundPresident = presidents.find(president => president.id === ctx.req.param('id'))
+  return foundPresident
+    ? ctx.json(foundPresident)
+    : ctx.json({ message: 'President not found' }, 404)
+})
+
+app.get('/static/*', serveStatic({ root: './' }))
 
 export default app
 
